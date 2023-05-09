77fcbf16f9dffb7fd3f4a80110de3f53c6e6c883
Albinmanoj1 <86677825+Albinmanoj1@users.noreply.github.com>
2023-05-10 00:37:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/Albinmanoj1/lallu.git /lallu
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /lallu
 fi
-cd /DQ-The-File-Donor
+cd /lallu
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting gogi...."
 python3 bot.py
